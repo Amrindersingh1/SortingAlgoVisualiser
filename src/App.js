5d4176e6c0e3b7392597403e9b77b280cdc5876f
Amrinder singh <amrinders433@gmail.com>
2020-04-05 10:33:31 -0400

    V1 App ready

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     super();
 
     this.state = {
-      numbers:  [3, 6, 5, 2, 1, 4, 8, 7, 9],
+      numbers: [3, 6, 5, 2, 1, 4, 8, 7, 9],
       colorArray: [
         "Azure",
         "Ivory",
@@ -19,11 +19,13 @@ export default class App extends Component {
         "Coral",
         "Fuchsia",
       ],
+      sleep: 1000,
     };
 
     this.BubbleSort = this.BubbleSort.bind(this);
     this.SelectionSort = this.SelectionSort.bind(this);
     this.InsertionSort = this.InsertionSort.bind(this);
+    this.QuickSort = this.QuickSort.bind(this);
   }
 
   sleep = (milliseconds) => {
@@ -36,7 +38,7 @@ export default class App extends Component {
 
     for (var i = 8; i >= 0; i--) {
       for (var j = 1; j <= i; j++) {
-        await this.sleep(1000);
+        await this.sleep(this.state.sleep);
         if (tArray[j - 1] > tArray[j]) {
           var temp = tArray[j - 1];
           tArray[j - 1] = tArray[j];
@@ -70,39 +72,86 @@ export default class App extends Component {
         numbers: tArray,
       });
 
-      await this.sleep(1000);
+      await this.sleep(this.state.sleep);
     }
   }
 
   async InsertionSort() {
     var tArray = this.state.numbers;
-    var i,
-      len = tArray.length,
-      el,
-      j;
-
-    for (i = 1; i < len; i++) {
-      el = tArray[i];
-      j = i;
-
-      while (j > 0 && tArray[j - 1] > tArray[j]) {
+    for (let i = 0; i < tArray.length; i++) {
+      let toCmp = tArray[i];
+      for (let j = i; j > 0 && toCmp < tArray[j - 1]; j--) {
         tArray[j] = tArray[j - 1];
-        j--;
+        tArray[j] = toCmp;
 
         this.setState({
           numbers: tArray,
         });
-
-        await this.sleep(1000);
+  
+        await this.sleep(this.state.sleep);
       }
-
-      tArray[j] = el;
     }
+  }
+
+  mergeSort = async(arr) => {
+    if (arr.length < 2) return arr;
+  
+    let middle = parseInt(arr.length / 2),
+    left = arr.slice(0, middle),
+    right = arr.slice(middle);
+    
+    await this.sleep(this.state.sleep); 
+    let temp = this.merge(this.mergeSort(left), this.mergeSort(right));
+    this.setState({
+      numbers: temp,
+    });
+
+  }
+  
+  merge = (left, right) => {
+    let result = [];
+  
+    while (left.length && right.length) {
+      left[0] <= right[0] ?
+      result.push(left.shift()) :
+      result.push(right.shift());
+    }
+  
+    while (left.length) result.push(left.shift());
+    while (right.length) result.push(right.shift());
+
+    
+  
+    return result;
+  }
+
+  async QuickSort(arr) {
+    if(arr.length <= 1) return arr;
+  
+    let pivot = Math.floor((arr.length -1)/2);
+    let val = arr[pivot], less = [], more = [];
+  
+    arr.splice(pivot, 1);
+    for(const e of arr){
+      e < val ? less.push(e) : more.push(e);
+      this.setState({
+        numbers: arr,
+      });
+
+      // await this.sleep(this.state.sleep);
+    };
+
+    let temp = [this.QuickSort(less),[val],this.QuickSort(more)];
+  
+    this.setState({
+      numbers: temp,
+    }); 
   }
 
   reset = () => {
     this.setState({
       numbers: [3, 6, 5, 2, 1, 4, 8, 7, 9],
+      sleep: 1000
     });
   };
 
@@ -123,11 +172,11 @@ export default class App extends Component {
           <button onClick={this.BubbleSort}>Bubble Sort</button>
           <button onClick={this.SelectionSort}>Selection Sort</button>
           <button onClick={this.InsertionSort}>Insertion Sort</button>
-          <button onClick={this.BubbleSort}>Merge Sort</button>
-          <button onClick={this.BubbleSort}>Quick Sort</button>
-          <button id="resetbtn" onClick={this.reset}>RESET</button>
+          
+          <button id="resetbtn" onClick={this.reset}>
+            RESET
+          </button>
         </div>
-        
       </div>
     );
   }
